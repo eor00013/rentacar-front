11ab0822c0d5d35b798432db7be949173a6174cd
Estibaliz <eor00013@gmail.com>
2021-02-17 18:56:29 +0100

    add more library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then((module) => module.HomeModule)
+    },
+
+    {
+        path: 'offerlist',
+        loadChildren: () => import('./modules/offers/offers.module').then((module) => module.OffersModule)
+    },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
