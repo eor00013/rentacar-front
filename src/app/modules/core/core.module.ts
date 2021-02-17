99c9b28d82700921ce0a89734fff5819454baaca
Estibaliz <eor00013@gmail.com>
2021-02-17 21:13:31 +0100

    add routerModule(library) in Coremodule

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent, ToolbarComponent } from './components';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ToolbarComponent, FooterComponent],
-    imports: [CommonModule, MatToolbarModule, HttpClientModule, FlexLayoutModule],
+    imports: [CommonModule, MatToolbarModule, HttpClientModule, FlexLayoutModule, RouterModule],
     exports: [ToolbarComponent, FooterComponent, FlexLayoutModule]
 })
 export class CoreModule {}
